3ad68265342b0f161afb290be916a5b75271615c
bchyi <bchyi@g.harvard.edu>
2026-04-10 09:12:56 -0400

    creating package

--- games/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from games.cards import pick_a_card
-from games.coins import flip_a_coin
-from games.dice import roll_a_die
-from games.lottery import generate_ticket
--- games/.ipynb_checkpoints/coins-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from numpy import random
-def flip_a_coin():
-    """this function flips a coin"""
-    print("crying")
-    return random.choice(['heads', 'tails'])
