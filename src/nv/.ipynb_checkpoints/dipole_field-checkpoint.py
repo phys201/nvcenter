f894d695cd72a363d427d51f344e21abce8f63ad
bchyi <bchyi@g.harvard.edu>
2026-04-17 09:07:39 -0400

    removed extraneous files

--- src/nv/.ipynb_checkpoints/dipole_field-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from numpy import random
-
-def pick_a_card():
-    """this function picks a card """
-    value = random.randint(1, 13)
-    suit = random.randint(4)
-    suit = ['clubs', 'diamonds', 'hearts', 'spades'][suit]
-    return (value, suit)
