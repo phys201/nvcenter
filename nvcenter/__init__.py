27c289e272de58001b81386e4719f62588d84f01
bchyi <bchyi@g.harvard.edu>
2026-05-06 05:07:23 -0400

    Dipole Model is soo close to converging

--- nvcenter/__init__.py
+++ nvcenter/__init__.py
@@ -1,2 +1,7 @@
 from nvcenter.odmr_analysis import *
 from nvcenter.data_io import *
+import numpy as np
+from numpy import random
+import pytensor.tensor as pt
+import pymc as pm
+import arviz as az
